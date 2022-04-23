aa751639ac48c53edf9fdc0dee114216ebf1a5d7
Youngjae Lee <skske1432@gmail.com>
2022-04-22 18:19:27 -0600

    404 page test

---
+++ pages/404.js
@@ -0,0 +1,35 @@
+import React from "react";
+import "./notFound.scss";
+
+function PageNotFound(props) {
+  return (
+    <>
+      <div className="back-ground">
+        <div className="stage">
+          <div className="layer"></div>
+          <div className="layer"></div>
+          <div className="layer"></div>
+          <div className="layer"></div>
+          <div className="layer"></div>
+          <div className="layer"></div>
+          <div className="layer"></div>
+          <div className="layer"></div>
+          <div className="layer"></div>
+          <div className="layer"></div>
+          <div className="layer"></div>
+          <div className="layer"></div>
+          <div className="layer"></div>
+          <div className="layer"></div>
+          <div className="layer"></div>
+          <div className="layer"></div>
+          <div className="layer"></div>
+          <div className="layer"></div>
+          <div className="layer"></div>
+          <div className="layer"></div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default PageNotFound;
