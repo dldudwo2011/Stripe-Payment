3c468e53c29d53a560e61f19e123a3ef9e1f5d25
Youngjae Lee <skske1432@gmail.com>
2022-04-22 18:47:14 -0600

    text 4

---
+++ pages/404/styles.js
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+
+const NotFoundStyles = styled.section`
+     text-align:center;
+     h1{
+         font-size:3rem;
+     }
+`;
+
+export{NotFoundStyles} ;
